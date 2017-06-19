9bdf9c1e764777639a0c3137d8fbccb780cb2215
Piotrek <piotrekk1@outlook.com>
2017-06-19 16:35:27 +0200

    Electron app can load text file

---
+++ src/renderer/components/MainPage.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <div>
+      {{fileContent}}
+    </div>
+    <button v-on:click="open">Click here</button>
+  </div>
+</template>
+
+<script>
+import fs from 'fs'
+import path from 'path'
+export default {
+  data: {
+    fileContent: 'none'
+  },
+  methods: {
+    open: function () {
+      console.log('test')
+      this.fileContent = fs.readFileSync(path.join(__static, '/someFile.txt'), 'utf8')
+      console.log(this.fileContent)
+    }
+  }
+}
+</script>
