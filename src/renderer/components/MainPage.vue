a5777b758fa21b2f8f7cefd24352e0ced5de22f7
Piotrek <piotrekk1@outlook.com>
2017-06-19 21:56:05 +0200

    Input automatically focused, enter sends command to runCommand function

--- src/renderer/components/MainPage.vue
+++ src/renderer/components/MainPage.vue
@@ -1,8 +1,12 @@
 <template>
   <div>
+    <input ref='commandLine' v-model='commandLineContent' v-on:keyup.13='runCommand' />
     <div>
       {{fileContent}}
     </div>
+    <p>
+      {{commandLineContent}}
+    </p>
     <button v-on:click='open'>Click here</button>
   </div>
 </template>
@@ -13,14 +17,27 @@ import path from 'path'
 export default {
   data: function () {
     return {
-      fileContent: ''
+      fileContent: '',
+      commandLineContent: ''
     }
+  },
+  mounted () {
+    window.addEventListener('focus', this.pageFocused)
   },
   methods: {
     open: function () {
       console.log('test')
       this.fileContent = fs.readFileSync(path.join(__static, '/someFile.txt'), 'utf8')
       console.log(this.fileContent)
+    },
+    runCommand () {
+      let currentCommand = this.$refs.commandLine.value
+      console.log(currentCommand)
+    },
+    pageFocused () {
+      console.log('pageFocused')
+      console.log(this)
+      this.$refs.commandLine.focus()
     }
   }
 }
